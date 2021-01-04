a85b63963382ecb754b6c37b1a954e8087838b49
mmoya26 <mmoya18@icloud.com>
2021-01-04 12:52:58 -0400

    Update testrail credentias

--- script.py
+++ script.py
@@ -5,8 +5,8 @@ from suite import Suite
 import argparse
 
 # API URL connection + ADMIN user credentials
-client = APIClient('https://mmoya18.testrail.io/')
-client.user = 'nexonmiguel26@gmail.com'
+client = APIClient('https://migueltestrail2.testrail.io/')
+client.user = 'migueltestrail1@outlook.com'
 client.password = 'Password123'
 
 # Command line arguments
